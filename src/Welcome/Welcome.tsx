d1f2f20032e8fdd3b7d4ebfa03f00871d2c2c56b
SeungminCha <dominico1225@gmail.com>
2025-01-10 22:23:51 +0900

    feat: style function init

--- src/Welcome/Welcome.tsx
+++ src/Welcome/Welcome.tsx
@@ -1,29 +1,24 @@
-import { Anchor, Text, Title } from "@mantine/core";
+import { Anchor, Text, Title } from '@mantine/core'
 
-import * as classes from "./Welcome.css";
+import * as s from './style.css'
 
 export function Welcome() {
   return (
-    <>
-      <Title className={classes.title} ta="center" mt={100}>
-        Welcome to{" "}
-        <Text
-          inherit
-          variant="gradient"
-          component="span"
-          gradient={{ from: "pink", to: "yellow" }}
-        >
+    <div className={s.Wrapper}>
+      <Title className={s.title} ta="center" mt={100}>
+        Welcome to{' '}
+        <Text inherit variant="gradient" component="span" gradient={{ from: 'pink', to: 'yellow' }}>
           Mantine
         </Text>
       </Title>
-      <Text color="dimmed" ta="center" size="lg" maw={580} mx="auto" mt="xl">
-        This starter Vite project includes a minimal setup, if you want to learn
-        more on Mantine + Vite integration follow{" "}
+      <Text c="dimmed" ta="center" size="lg" maw={580} mx="auto" mt="xl">
+        This starter Vite project includes a minimal setup, if you want to learn more on Mantine + Vite integration
+        follow{' '}
         <Anchor href="https://mantine.dev/guides/vite/" size="lg">
           this guide
         </Anchor>
         . To get started edit pages/Home.page.tsx file.
       </Text>
-    </>
-  );
+    </div>
+  )
 }
