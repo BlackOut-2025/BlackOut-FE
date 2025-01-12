11e243c1a597f39c1ec5d91e56a527e10f3cc80d
SeungminCha <dominico1225@gmail.com>
2025-01-12 09:51:47 +0900

    feat: 디자인 피드백 반영

--- src/features/Concert/ConcertReservation/index.tsx
+++ src/features/Concert/ConcertReservation/index.tsx
@@ -3,11 +3,23 @@ import { Button } from '@mantine/core'
 import * as s from './style.css'
 
 import { vars } from '@/theme'
+import { useFlow } from '@stackflow/react/future'
 
 const ConcertReservationButton = () => {
+  const { push, pop } = useFlow()
   return (
     <div className={s.Wrapper}>
-      <Button fullWidth bg={vars.colors.gray[8]} size="lg" fw={600} radius="xl">
+      <Button
+        fullWidth
+        bg={vars.colors.gray[8]}
+        size="lg"
+        fw={600}
+        radius="xl"
+        onClick={() => {
+          push('MyTicketPage', {})
+          pop()
+        }}
+      >
         예매하기
       </Button>
     </div>
--- src/features/Home/Category/index.tsx
+++ src/features/Home/Category/index.tsx
@@ -1,6 +1,7 @@
 import { Button } from '@mantine/core'
 
 import * as s from './style.css'
+import { vars } from '@/theme'
 
 const HomeCategory = () => {
   return (
@@ -9,16 +10,16 @@ const HomeCategory = () => {
         <Button size="xs" radius="xl" color="dark.7">
           콘서트
         </Button>
-        <Button size="xs" radius="xl" color="gray.2">
+        <Button size="xs" radius="xl" color="gray.2" style={{ color: vars.colors.gray[5] }}>
           뮤지컬
         </Button>
-        <Button size="xs" radius="xl" color="gray.2">
+        <Button size="xs" radius="xl" color="gray.2" style={{ color: vars.colors.gray[5] }}>
           연극
         </Button>
-        <Button size="xs" radius="xl" color="gray.2">
+        <Button size="xs" radius="xl" color="gray.2" style={{ color: vars.colors.gray[5] }}>
           클래식/무용
         </Button>
-        <Button size="xs" radius="xl" color="gray.2">
+        <Button size="xs" radius="xl" color="gray.2" style={{ color: vars.colors.gray[5] }}>
           팬클럽/팬미팅
         </Button>
       </div>
