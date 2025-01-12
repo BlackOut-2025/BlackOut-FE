b9011d86bbfb99f19be6f3e217010301392e6976
SeungminCha <dominico1225@gmail.com>
2025-01-12 09:31:43 +0900

    feat: 페이지 연결

--- src/features/Home/TicketList/TicketItem.tsx
+++ src/features/Home/TicketList/TicketItem.tsx
@@ -3,10 +3,12 @@ import { Image, Text } from '@mantine/core'
 import * as s from './style.css'
 
 import BlackSkirts from '@/common/assets/BlackSkirts.png'
+import { useFlow } from '@stackflow/react/future'
 
 const TicketItem = () => {
+  const { push } = useFlow()
   return (
-    <div className={s.TicketItemWrapper}>
+    <div className={s.TicketItemWrapper} onClick={() => push('ConcertDetailPage', { concertId: '1234' })}>
       <Image src={BlackSkirts} className={s.Image} />
       <div className={s.Content}>
         <div className={s.Text}>
